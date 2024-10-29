b8603637ff2402a35b0437a6f8863358a14cae6b
KimMinam06 <minam.kim@merakiplace.kr>
2024-10-29 17:28:51 +0900

    Feat. Connect to FireBase and insert Music Chart - Firebase와 연동 - rank 변수 전역변수로 설정 - vibe 음원사이트 추가 - 셀레니움이 필요한 음원사이트 주석 표시 - Firebase SDK 환경변수 설정

--- scrapy.py
+++ scrapy.py
@@ -1,5 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+load_dotenv('env/.env')
+
+cred = credentials.Certificate(os.environ.get('CERTIFICATE'))
+firebase_admin.initialize_app(cred, {
+    'databaseURL': os.environ.get('DB_URLS')
+})
 
 urls = {
     "melon": {
@@ -14,11 +26,17 @@ urls = {
          "title" : "ytmc-v2-app > ytmc-detailed-page > div:nth-of-type(2) > ytmc-chart-table-v2 > div > ytmc-entry-row > div > div > div:nth-of-type(2) > div > div:nth-of-type(1)",
          "artist" : "ytmc-v2-app > ytmc-detailed-page > div:nth-of-type(2) > ytmc-chart-table-v2 > div > ytmc-entry-row > div > div > div:nth-of-type(1) > span"
          },
-    "genie": {
+    "genie": { # 스크래핑은 되지만 50위 까지만 긁어와짐 (결론 : 셀레니움 필요)
               "link":"https://www.genie.co.kr/chart/top200?ditc=D&ymd=20241026&hh=21&rtm=Y&pg=1",
               "chart_list" : "tbody > tr",
               "title" : ".info > .title",
               "artist" : ".info > .artist"
+    },
+    "vibe" : { # 셀레니움 필요
+      "link" :"https://vibe.naver.com/chart/total",
+      "chart_list" : "tbody > tr",
+      "title": "div.title_badge_wrap > span",
+      "artist": "div.artist_sub > span"
     },
     "bugs" : {
       "link" :"https://music.bugs.co.kr/chart",
@@ -33,16 +51,16 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
-rank = 0
-
-
-def scrapy(chart_list, site_info, rank) :
+def scrapy(chart_list, site_info, site_name) :
+    global rank
+    rank = 0
     for rankNum, song in enumerate(chart_list[rank:100], 1):
             title = song.select_one(site_info["title"]).text.strip()
             artist = song.select_one(site_info["artist"]).text.strip()
             rank = rankNum
-            print(f"{rankNum}. {title} - {artist}")
-    print(f"Rank : {rank}")
+            dir = db.reference(site_name)
+            dir.update({rank: f"{title} - {artist}"})
+            print(f"{rank}. {title} - {artist}")
     return rank
 
 def crawl(site_name):
@@ -51,11 +69,9 @@ def crawl(site_name):
         return
     site_info = urls[site_name]
     site_link = site_info["link"]
-    
     res = requests.get(site_link, headers=headers)
     soup = BeautifulSoup(res.text, 'html.parser')
     chart_list = soup.select(site_info["chart_list"])
-    scrapy(chart_list, site_info, rank)
+    scrapy(chart_list, site_info, site_name)
 
-crawl("youtubeMusic")
-
+crawl("bugs")
