2d0bf051d1c577373ae53df0064e767b2b2cf81a
Kimminam <bluemind8520@gmail.com>
2024-10-31 21:57:07 +0900

    Add. cred elements

--- scrapy.py
+++ scrapy.py
@@ -1,14 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv
 import os 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-load_dotenv('env/.env')
-
-cred = credentials.Certificate(os.environ.get('CERTIFICATE'))
+cred = credentials.Certificate({
+    "type": os.environ.get('type'),
+    "project_id": os.environ.get('project_id'),
+    "private_key_id": os.environ.get('private_key_id'),
+    "private_key": os.environ.get('private_key').replace('\\n', '\n'),
+    "client_email": os.environ.get('client_email'),
+    "client_id": os.environ.get('client_id'),
+    "auth_uri": os.environ.get('auth_uri'),
+    "token_uri": os.environ.get('token_uri'),
+    "auth_provider_x509_cert_url": os.environ.get('auth_provider_x509_cert_url'),
+    "client_x509_cert_url": os.environ.get('client_x509_cert_url'),
+    "universe_domain": os.environ.get('universe_domain')
+})
 firebase_admin.initialize_app(cred, {
     'databaseURL': os.environ.get('DB_URLS')
 })
